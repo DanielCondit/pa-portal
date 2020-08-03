c003f47c1dd6024f1e40004204e9011ae8ccdde0
danielcondit <danieljcondit@gmail.com>
2020-08-03 07:56:39 -0500

    Updatign some styles

---
+++ public/assets/scripts/email.js
@@ -0,0 +1,30 @@
+$(document).ready(function () {
+
+  $("#submit_contact").click(function() {
+    var name = $("#contact_name").val();
+    var email = $("#contact_email").val(); 
+    var message = $("#contact_message").val();
+    var contact = $("#contact_phone").val();
+    $("#returnmessage").empty(); // To empty previous error/success message.
+    // Checking for blank fields.
+    if (name == '' || email == '' || contact == '') {
+      alert("Please Fill Required Fields");
+    } else {
+      // Returns successful data submission message when the entered information is stored in database.
+      $.ajaxSetup({async: true});
+      $.post("contact_form.php", {
+        async: true,
+        name1: name,
+        email1: email,
+        message1: message,
+        contact1: contact
+      }, function(data) {
+        $("#returnmessage").append(data); // Append returned message to message paragraph.
+        if (data == "Your Query has been received, We will contact you soon.") {
+          $("#contact_form")[0].reset(); // To reset form fields on success.
+        }
+      });
+    }
+  });
+  
+});
